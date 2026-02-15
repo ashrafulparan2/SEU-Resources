c289f387f51089fde42fb12386632e64ff17808f
ashrafulparan2 <ashrafulparan@gmail.com>
2026-02-15 22:55:44 +0600

    update java lab

--- CSE282/Code/Week 2/DoWhileExample.java
+++
@@ -1,9 +0,0 @@
-public class DoWhileExample {
-    public static void main(String[] args) {
-        int i = 1;
-        do {
-            System.out.println("Value: " + i);
-            i++;
-        } while (i <= 5);
-    }
-}
